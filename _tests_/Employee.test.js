ab63f368af8ebeb770282305589594cc80a84755
TemplarManatee <dylanbrad10@gmail.com>
2023-01-05 19:37:23 -0500

    Started working on tests, classes are set up

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,3 +1,17 @@
 const Employee = require('../lib/Employee'); 
 
-describe()
+describe('Employee Class', () => {
+    it('Takes in 3 parameters: name, id, and email', () => {
+        const freddy = new Employee('Freddy', 1, 'freddytodd@frogs.com');
+        
+        expect(freddy.name).toBe('Freddy');
+        expect(freddy.id).toBe(1);
+        expect(freddy.email).toBe('freddytodd@frogs.com');
+    });
+
+    it("Returns the role of 'Employee'", () => {
+        const amanda = new Employee('Amanda', 2, 'edgarallenslowbro@gmail.com');
+
+        expect (amanda.role).toBe('Employee');
+    });
+})
