16f923834da6f56e4010d569eea6d7d4273ec27a
DylanF <dylanbrad10@gmail.com>
2023-03-31 16:26:00 -0400

    Final changes

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# challenge10
-Object-Oriented Programming: Team Profile Generator
+# Employee Information Page
+
+![badge](https://shields.io/badge/license-MIT-green)
+
+## Description
+
+A CLI application that generates an HTML view of all employees
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Screenshots](#screenshots)
+- [Demo](#demo)
+- [License](#license)
+
+## Installation
+
+Clone the repository, run `npm install`, then run `node index.js`
+
+## Usage
+
+Enter the manager's information first, then enter employee information for either Engineers or Interns. When you're satisfied with the number of employees you've entered, an html file will be generated in the dist folder.
+
+## Screenshot
+
+![image](Screenshot.png)
+
+## Demo
+
+https://watch.screencastify.com/v/FVidlqpGnlbsQZQFhz8S
+
+## License
+
+This project is covered under the MIT license.
+
+## Contact Me
+
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white) [TemplarManatee](https://github.com/TemplarManatee/)
+
+You can reach me at dylanbfreeman@gmail.com with any questions!
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,44 +1,36 @@
-function managerCard (managerData) {
-  return (`<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">${managerData.name}</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Manager</a>
-      <p class="mt-2 text-gray-500"><strong>Email:</strong> ${managerData.email}</p>
-      <p class="mt-2 text-gray-500"><strong>Office:</strong> ${managerData.officeNumber}</p>
-    </div>
-  </div>
-</div>`)};
+function managerCard(managerData) {
+  return `
+<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-12">
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${managerData.name}</h5>
+  <p class="font-normal text-gray-700 dark:text-gray-400"><strong>Manager (id: ${managerData.id})</strong></a>
+  <p class="font-normal text-gray-700 dark:text-gray-400">Email: ${managerData.email}</p>
+  <p class="font-normal text-gray-700 dark:text-gray-400">Office: ${managerData.officeNumber}</p>
+</div>`;
+}
 
-function engineerCard (engineerData) {
-  return `<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">${engineerData.name}</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Engineer</a>
-      <p class="mt-2 text-gray-500"><strong>Email:</strong> ${engineerData.email}</p>
-      <p class="mt-2 text-gray-500"><strong>GitHub:</strong> ${engineerData.github}</p>
-    </div>
-  </div>
-</div>`
-};
+function engineerCard(engineerData) {
+  return `
+<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-12">
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${engineerData.name}</h5>
+  <p class="font-normal text-gray-700 dark:text-gray-400"><strong>Intern (id: ${engineerData.id})</strong></a>
+  <p class="font-normal text-gray-700 dark:text-gray-400">Email: ${engineerData.email}</p>
+  <p class="font-normal text-gray-700 dark:text-gray-400">GitHub: ${engineerData.github}</p>
+</div>`;
+}
 
 const internCard = (internData) => {
-  return `<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">${internData.name}</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Intern</a>
-      <p class="mt-2 text-gray-500"><strong>Email:</strong> ${internData.email}</p>
-      <p class="mt-2 text-gray-500"><strong>School:</strong> ${internData.school}</p>
-    </div>
-  </div>
-</div>`
+  return `
+<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-12">
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${internData.name}</h5>
+  <p class="font-normal text-gray-700 dark:text-gray-400"><strong>Intern (id: ${internData.id})</strong></a>
+  <p class="font-normal text-gray-700 dark:text-gray-400">Email: ${internData.email}</p>
+  <p class="font-normal text-gray-700 dark:text-gray-400">School: ${internData.school}</p>
+</div>`;
 };
 
-// generate html page 
-const generateFullSite = function (employeeCards) {   
-  return`
+// generate html page
+const generateFullSite = function (employeeCards) {
+  return `
   <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -52,14 +44,10 @@ const generateFullSite = function (employeeCards) {
       <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      <header>
-          <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
-          </nav>
-      </header>
+      <header class="justify-center m-4  text-2xl font-bold">Team Profile Generator</header>
       <main>
           <div class="container">
-              <div class="row justify-content-center" id="team-cards">
+              <div class="flex justify-center" id="team-cards">
                   <!--Team Cards-->
                   ${employeeCards}
               </div>
@@ -71,41 +59,39 @@ const generateFullSite = function (employeeCards) {
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   </html>
 `;
-}
+};
 
 const generateHtml = (data) => {
-  cardArray = []; 
+  cardArray = [];
 
   for (let i = 0; i < data.length; i++) {
-      const employee = data[i];
-      const role = employee.getRole(); 
+    const employee = data[i];
+    const role = employee.getRole();
 
-      if (role === 'Manager') {
-          const manager = managerCard(employee);
-          cardArray.push(manager);
-      }
+    if (role === "Manager") {
+      const manager = managerCard(employee);
+      cardArray.push(manager);
+    }
 
-      // call engineer function
-      if (role === 'Engineer') {
-          const engineer = engineerCard(employee);
-          cardArray.push(engineer);
-      }
+    // call engineer function
+    if (role === "Engineer") {
+      const engineer = engineerCard(employee);
+      cardArray.push(engineer);
+    }
 
-      // call intern function 
-      if (role === 'Intern') {
-          const intern = internCard(employee);
-          cardArray.push(intern);
-      }
-      
+    // call intern function
+    if (role === "Intern") {
+      const intern = internCard(employee);
+      cardArray.push(intern);
+    }
   }
 
-  // joining strings 
-  const employeeCards = cardArray.join('')
+  // joining strings
+  const employeeCards = cardArray.join("");
 
   // return to generated page
-  const generateTeam = generateFullSite(employeeCards); 
+  const generateTeam = generateFullSite(employeeCards);
   return generateTeam;
-
-}
+};
 
 module.exports = generateHtml;
